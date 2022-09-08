397f00bb92762b7561e5cd23ecdc51c14e3fc5ed
Aleksandra Badaczewska <dawid.aleksandra@gmail.com>
2022-09-08 09:58:05 -0500

    updated script to generate new Master Excel

--- README.md
+++ README.md
@@ -8,33 +8,44 @@ optional arguments:<br>
   -h,          --help                         # show this help message and exit
   -l labels,   --labels-filename labels       # [string] input 2-col file in the xlsx format; required
   -f data,     --data-file data               # [string] input multi-col file in the xlsx format; required
+  -t samples,  --master-file samples          # [string] input Master Excel (samples) file in the xlsx format; optional
   -c col,      --label-column col             # [string] index of the label column in the data_file; default=0
   -o format,   --output-format format         # select format for output file: 0 - xlsx, 1 - csv; default=0
-  -n outfile,  --output-filename outfile      # provide custom name for the output file; default="output"
+  -n outfile,  --output-filename outfile      # provide custom name for the output file; default="data_output"
+  -m master,   --output-masterfile master     # provide custom name for the output master_file; default: "master_output"
 ```
 
 USAGE:<br>
 
 ```
-data_matcher.py [-h] -l labels -f data [-c col] [-o format] [-n outfile]
+data_matcher.py [-h] -l labels -f data [-t samples] [-c col] [-o format] [-n outfile] [-m master]
 ```
 
-minimal arguments:<br>
+* minimal arguments to generate data_output:<br>
 
 ```
 python3 data_matcher.py -l labels.xlsx -f data.xlsx
 ```
 
-OUTPUT:
+* minimal arguments to generate data_output and master_output:<br>
 
+```
+python3 data_matcher.py -l labels.xlsx -f data.xlsx -t samples.xlsx
+```
+
+**data_output**<br>
 The exact copy of the input datafile [*data.xlsx*] with one additional column (next to the ARS label) is created [*output.xlsx* by default]. The new column contains the corresponding sample label. If there is no matching sample label, the error value -f -9999.99 is assigned.
+
+**master_output**<br>
+
+
 
 
 Files description:<br>
 
 |file | description|
 |-----|------------|
-|Original_Sample_Master_List.xlsx| original **sample labels** of experimental data |
+|Original_Sample_Master_List.xlsx **[samples.xlsx]** | original **sample labels** of experimental data |
 |F-set_complete.xlsx **[labels.xlsx]**| list of matching labels pairs; 2-column file |
 |Sample_Data_by_ARS_F-number.xlsx| data annotated with **ARS label** |
 |Sample_check_by_F-number.xlsx **[data.xlsx]**| data annotated with **ARS label** |
--- data_matcher.py
+++ data_matcher.py
@@ -6,11 +6,13 @@ import pandas as pd		# to easily parse json object and filter out data; require 
 from datetime import datetime	# to create unique tag into the default output filename
 
 
-def convert_jsonp_to_json(label_file, data_file, label_col, output_format, outfile):
+def convert_jsonp_to_json(label_file, data_file, samples_file, label_col, output_format, outfile, outmaster):
 
+    tag = datetime.now().strftime("-%d-%m-%Y-%H%M%S")
 ###-- read inputs and create data structure
     labs = pd.read_excel(label_file, index_col=None, header=0)	# read xlsx file with pairs of matched labales (labels.xlsx, e.g., F-set_complete.xlsx)
     data = pd.read_excel(data_file, index_col=None, header=0)	# read xlsx file with data annotated by single label (data.xlsx, e.g., Sample_check_by_F-number.xlsx)
+    cols = data.columns.tolist()                                # list of columns names in the data_file
     dlab = data.columns.tolist()[label_col]			# header of the label column in data_file
     lab1 = ''							# header of the column of the known label in label_file
     lab2 = ''							# header of the column of the other (matching) label in label_file
@@ -22,7 +24,7 @@ def convert_jsonp_to_json(label_file, data_file, label_col, output_format, outfi
     	    lab2 = i						# if test_label is not found in column, it is matching label
 #    print(dlab, lab1, lab2)
 
-###-- assign matching labels
+###-- assign matching labels in datafile
     data.insert(label_col+1, lab2, '')				# insert new label column into data (next to known label)				
     for idx, i in data[dlab].iteritems():
         val = labs[labs[lab1] == i]				# find matching row in labels dataframe
@@ -31,10 +33,33 @@ def convert_jsonp_to_json(label_file, data_file, label_col, output_format, outfi
         else:
             data.at[idx, lab2] = -9999.99			# assign error val if no matching label    
 #    print(data)
+
+###-- transfer data into Master Excel
+    if samples_file != '':
+        master = pd.read_excel(samples_file, index_col=None, header=0)
+        header = master.columns.tolist()[0]
+        [master.insert(idx+1, i, '') for idx, i in enumerate(cols)]
+        for idx, i in master[header].iteritems():
+            val = data[data[lab2] == i]
+            if len(val) > 0:
+                for col in cols:
+                    master.at[idx, col] = val[col].values[0]
+            else:
+                for col in cols:
+                    master.at[idx, col] = -9999.99
+#        print(master)
+
+        if outmaster == 'master_output':
+            outmaster += tag
+        if output_format == 1:
+            master.to_csv(outmaster+'.csv', sep=',', encoding='utf-8')     # output in CSV format
+        else:					
+            master.to_excel(outmaster+'.xlsx', index=False, header=True)   # output in xlsx format (Excel)        
+                
             
-###-- export output file
-    if outfile == 'output':
-        outfile += datetime.now().strftime("-%d-%m-%Y-%H%M%S")
+###-- export output data_file
+    if outfile == 'data_output':
+        outfile += tag
     if output_format == 1:
         data.to_csv(outfile+'.csv', sep=',', encoding='utf-8')     # output in CSV format
     else:					
@@ -65,6 +90,13 @@ if __name__ == '__main__':
         required=True
     )
     parser.add_argument(
+        '-t', '--master-file',
+        help='[string] input Master Excel (samples) file in the xlsx format',
+        metavar='samples',
+        default='',
+        dest='samples'
+    )
+    parser.add_argument(
         '-c', '--label-column',
         help='[string] index of the label column in the data_file',
         metavar='col',
@@ -81,12 +113,20 @@ if __name__ == '__main__':
         dest='out'
     )
     parser.add_argument(
-        '-n', '--output-filename',
-        help='provide custom name for the output file',
+        '-n', '--output-datafile',
+        help='provide custom name for the output data_file',
         metavar='outfile',
         type=str,
-        default='output',
+        default='data_output',
         dest='outfile'
+    )
+    parser.add_argument(
+        '-m', '--output-masterfile',
+        help='provide custom name for the output master_file',
+        metavar='master',
+        type=str,
+        default='master_output',
+        dest='master'
     )
 
 ###-- print example of usage and help message when script is run without required arguments
@@ -96,4 +136,4 @@ if __name__ == '__main__':
         sys.exit(1)
 
     args = parser.parse_args()
-    convert_jsonp_to_json(args.labels, args.data, args.col, args.out, args.outfile)
+    convert_jsonp_to_json(args.labels, args.data, args.samples, args.col, args.out, args.outfile, args.master)
